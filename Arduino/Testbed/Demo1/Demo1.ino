7ab33c1f6b2844b9fe54ff9b1d7f684ed80b3b69
Pef <paul-emmanuel.faidherbe@valtech.fr>
2017-06-03 12:59:35 +0200

    Fixes #13 : demos

--- Arduino/Testbed/Demo1/Demo1.ino
+++ Arduino/Testbed/Demo1/Demo1.ino
@@ -9,15 +9,35 @@
 
 #include <Servo.h>
 
+#define PIN_ARBRE 2
+
 Servo myservo;  // create servo object to control a servo 
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  pinMode(PIN_ARBRE,OUTPUT);
 }
 
 void loop() {
+  // porte
   myservo.write(20);
   delay(2000);
+
+  // arbre
+  digitalWrite(PIN_ARBRE,HIGH);
+  delay(1000);
+  digitalWrite(PIN_ARBRE,LOW);
+  delay(1000);
+  digitalWrite(PIN_ARBRE,HIGH);
+  delay(1000);
+  digitalWrite(PIN_ARBRE,LOW);
+  delay(1000);
+  digitalWrite(PIN_ARBRE,HIGH);
+  delay(1000);
+  digitalWrite(PIN_ARBRE,LOW);
+  delay(1000);
+
+  // porte
   myservo.write(70);
   delay(2000);
 }
